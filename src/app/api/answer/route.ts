9a9ec2bed310a24716f8d9dcfad6fc736d843ff6
mehulpal12 <89767204+mehulpal12@users.noreply.github.com>
2025-12-26 21:49:58 +0530

    done

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -6,52 +6,74 @@ import { UserPrefs } from "@/store/Auth";
 import { users } from "@/models/server/config";
 
 export async function POST(request: NextRequest) {
-    
-    try {
-        const {questionId, answer, authorId } =  await request.json();
+  try {
+    const { questionId, answer, authorId } = await request.json();
 
-       const response = await databases.createDocument(db, answerCollection, ID.unique(),{
+    const response = await databases.createDocument(
+      db,
+      answerCollection,
+      ID.unique(),
+      {
         content: answer,
         authorId: authorId,
         questionId: questionId,
-       })
+      }
+    );
+    // increase the reputaion
+    const prefs = await users.getPrefs<UserPrefs>(authorId);
 
-      const prefs =await users.getPrefs<UserPrefs>(authorId)
+    await users.updatePrefs(authorId, {
+      reputation: Number(prefs.reputation) + 1,
+    });
 
-      await users.updatePrefs(authorId, {
-        reputation: Number(prefs.reputation) + 1,
-      })
-
-      return NextRequest.json(response, {
-        status: 201
-      })
-
-
-    } catch (error: any) {
-        return NextResponse.json(
-            {
-                error:error?.message || "error createing answer"
-            },
-            {
-                status: error?.status || error?.code || 500
-            }
-        )
-    }
-
-
+    return NextResponse.json(response, {
+      status: 201,
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error?.message || "error createing answer",
+      },
+      {
+        status: error?.status || error?.code || 500,
+      }
+    );
+  }
 }
 
 export async function DELETE(request: NextRequest) {
-    try {
-        
-    } catch (error: any) {
-         return NextResponse.json(
-            {
-                error:error?.message || "error createing answer"
-            },
-            {
-                status: error?.status || error?.code || 500
-            }
-        )
-    }
+  try {
+    const { answerId } = await request.json();
+    const answer = await databases.getDocument(db, answerCollection, answerId);
+    const response = await databases.deleteDocument(
+      db,
+      answerCollection,
+      answerId
+    );
+
+    // decrease the reputaion
+    const prefs = await users.getPrefs<UserPrefs>(answer.authorId);
+
+    await users.updatePrefs(answer.authorId, {
+      reputation: Number(prefs.reputation) - 1,
+    });
+
+    return NextResponse.json(
+      {
+        data: response,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error?.message || "error createing answer",
+      },
+      {
+        status: error?.status || error?.code || 500,
+      }
+    );
+  }
 }
